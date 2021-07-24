cf07bbef82ed40347a14ec1a52ff45726d4bfb15
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-07-25 05:07:43 +0530

    8

---
+++ 8.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+    int n, sum=0,temp,count=0,x=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        sum+=arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+
+        for(int j=0;j<n-i-1;j++){
+
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+
+        }
+    }
+    while(sum>=x){
+        x+=arr[n-1];
+        sum-=arr[n-1];
+        count++;
+        n--;
+    }
+cout<<count;
+
+
+}
