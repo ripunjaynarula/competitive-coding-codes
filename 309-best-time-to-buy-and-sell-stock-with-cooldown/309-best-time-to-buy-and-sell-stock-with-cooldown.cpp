e0b6aa57114e0ec4ab619ceb888690d1f454498b
Ripunjay Narula <ripunjay30narula@gmail.com>
2022-05-01 03:57:01 +0530

    Time: 13 ms (22.16%), Space: 11.8 MB (34.82%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
-int maxProfit(vector<int> prices) {
-int n=prices.size();
-
-    vector<int>v(n,0);
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(i==0 || i==1){
-                v[j]=max(v[j],prices[j]-prices[i]);
-                v[j]=max(v[j],v[j-1]);
-            }
-            else{
-                v[j]=max(v[j],prices[j]-prices[i]+v[i-2]);
-                v[j]=max(v[j],v[j-1]);
+    
+    int trading(int i, int buy, vector<int> &values, vector<vector<int> > &dp, int n) {
+	if(i >= n) {
+		return 0;
+	}
+	
+	if(dp[i][buy] != -1) return dp[i][buy];
+	
+	int profit = 0;
+	if(buy) {
+		            int a = (-values[i] + trading(i+1, 0, values, dp, n)); 
+				 int b = trading(i+1, 1, values, dp, n);
+        if(a > b)
+            profit = a;
+        else profit = b;
+	}
+	else {
+		int a = (+values[i] + trading(i+2, 1, values, dp, n)); 
+				int b =	trading(i+1, 0, values, dp, n);
+        if(a > b) profit = a;
+        else profit = b;
+	}
+	return dp[i][buy] = profit;	
+	
+}
+    
+    int maxProfit(vector<int>& values) {
+        int n = values.size();
+        vector<vector<long>> dp(n+2, vector<long>(2, 0));
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        dp[n+1][0] = 0;
+        dp[n+1][1] = 0;
+        for(int i = n-1; i >=0; i-- ) {
+            for(int buy = 0; buy < 2 ; buy++) {
+                if(buy) {
+                    dp[i][buy] = max((-values[i] + dp[i+1][0]), 
+                                dp[i+1][1]);
+                }
+                else {
+                    dp[i][buy] = max((+values[i] + dp[i+2][1]), 
+                                dp[i+1][0]);
+                }
             }
         }
-    }
-    
-    int ans=0;
-    for(int i=0;i<n;i++){
-        ans=max(ans,v[i]);
-    }
-    return ans;
-}
+        return dp[0][1];
+         }
 };
