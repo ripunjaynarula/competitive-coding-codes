29f9b9c62f55064a749bdb02586b65052b0f6ae8
Ripunjay Narula <ripunjay30narula@gmail.com>
2022-06-02 04:39:31 +0530

    Time: 25 ms (59.14%), Space: 19.4 MB (92.48%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,7 +3,6 @@ public:
     int majorityElement(vector<int>& nums) {
        int n=nums.size();
         int count=0;
-        sort(nums.begin(),nums.end());
         int max=INT_MIN;
         for(int i=0;i<n;i++){
             if (count == 0) {
