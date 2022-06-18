5cb534bde81a00ad583858708d8af13c3de007cf
Ripunjay Narula <ripunjay30narula@gmail.com>
2022-06-19 02:31:17 +0530

    Time: 49 ms, Memory: 24.9 MB - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        
+		//remove redundant k values  and shorten k
         k = k % nums.size();
+        
+		//reverse the array from starting to end
         reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+		
+		//reverse from starting till kth element
+        reverse(nums.begin(), nums.begin()+k);
+		
+		//reverse from kth till last element
+        reverse(nums.begin()+k, nums.end());
+        
     }
 };
