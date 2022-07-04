35906a1f11631c6b8c048e3df541f4de63926611
Ripunjay Narula <ripunjay30narula@gmail.com>
2022-07-05 02:23:38 +0530

    Time: 7 ms (97.75%), Space: 7.1 MB (82.16%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -8,11 +8,11 @@ string longestPalindrome(string s) {
     int min_start = 0, max_len = 1;
     
     for (int i = 0; i < s.size();) {
-      if (s.size() - i <= max_len / 2) 
-      {
-          cout<<s.size() - i <<" "<< max_len / 2;
-          // break;
-      }
+      // if (s.size() - i <= max_len / 2) 
+      // {
+      //     cout<<s.size() - i <<" "<< max_len / 2;
+      //     // break;
+      // }
         
       int j = i, k = i;
       while (k < s.size()-1 && s[k+1] == s[k])
