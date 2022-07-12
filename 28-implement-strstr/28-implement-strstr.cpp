bf7824b2c93cd637ccd8599b1e9d5b336d2d9ebe
Ripunjay Narula <ripunjay30narula@gmail.com>
2022-07-12 22:26:47 +0530

    Time: 2 ms (52.43%), Space: 6.2 MB (61.19%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+        if(needle.size() == 0) return 0;
+        unordered_map<char, int>m;
+        for(auto x: needle) m[x]++;
+        int count = needle.size();
+        int begin = 0, end = 0;
+        while(end < haystack.size()){
+            if(m[haystack[end++]]-- > 0) count--;
+            if(count == 0){
+                int i = begin, j = 0;
+                while(j < needle.size() && haystack[i] == needle[j]) i++, j++;
+                if(j == needle.size()) return begin;
+            }
+            if(end - begin == needle.size() && m[haystack[begin++]]++ >= 0) count++;
+        }
+        return -1;
     }
 };
