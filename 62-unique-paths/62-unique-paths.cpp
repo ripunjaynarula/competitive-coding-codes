0e3272a8651fa22445814a45165e55000b94d452
Ripunjay Narula <ripunjay30narula@gmail.com>
2022-08-01 23:05:54 +0530

    Time: 12 ms (7.14%), Space: 6.1 MB (59.42%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,7 +3,10 @@ public:
     int uniquePaths(int m, int n) {
         vector<int> cur(n, 1);
         for (int i = 1; i < m; i++) {
+            cout<<"Row"<<i<<endl;
             for (int j = 1; j < n; j++) {
+            cout<<"Column"<<i;
+
                 cur[j] += cur[j - 1];
             }
         }
